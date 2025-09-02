fdbe6a38da3898c1c64c42310a052a33bd115bc5
viannik <ivan.nikitski@gmail.com>
2025-09-02 19:19:56 +0300

    formatting fix

--- cinema/urls.py
+++ cinema/urls.py
@@ -41,14 +41,46 @@ cinema_hall_detail = CinemaHallViewSet.as_view(
 
 
 urlpatterns = [
-    path("genres/", GenreList.as_view(), name="genre-list"),
-    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
-    path("actors/", ActorList.as_view(), name="actor-list"),
-    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path(
+        "genres/",
+        GenreList.as_view(),
+        name="genre-list",
+    ),
+    path(
+        "genres/<int:pk>/",
+        GenreDetail.as_view(),
+        name="genre-detail",
+    ),
+    path(
+        "cinema_halls/",
+        cinema_hall_list,
+        name="cinema-hall-list",
+    ),
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail",
+    ),
+    path(
+        "actors/",
+        ActorList.as_view(),
+        name="actor-list",
+    ),
+    path(
+        "actors/<int:pk>/",
+        ActorDetail.as_view(),
+        name="actor-detail",
+    ),
+    path(
+        "movies/",
+        movie_list,
+        name="movie-list",
+    ),
+    path(
+        "movies/<int:pk>/",
+        movie_detail,
+        name="movie-detail",
+    ),
 ]
 
 app_name = "cinema"
